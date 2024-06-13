f512bba818eff8392c1a851b33345b0f79d2ffb5
fpresenza <francisco.presenza@gmail.com>
2024-06-13 17:22:06 -0300

    a few more assertions

--- stark101/src/main.rs
+++ stark101/src/main.rs
@@ -1,10 +1,12 @@
+use lambdaworks_math::field::traits::IsFFTField;
 use lambdaworks_math::field::fields::fft_friendly::stark_252_prime_field::Stark252PrimeField;
 use lambdaworks_math::field::element::FieldElement;
 use lambdaworks_math::polynomial::Polynomial;
 
 // the stark252 field has 2-adicity of 192, i.e., the largest
 // multiplicative subgroup whose order is a power of two has order 2^192
-type FE = FieldElement<Stark252PrimeField>;
+type F = Stark252PrimeField;
+type FE = FieldElement<F>;
 
 // interpolation domain of size 1024 = 2^10
 const INT_DOM_SIZE: usize = 0b10000000000;
@@ -25,9 +27,11 @@ fn main() {
     } 
 
     // fft-interpolate the fibonacci square sequence
-    let trace_poly = match Polynomial::interpolate_fft::<Stark252PrimeField>(&fib_sq) {
+    let trace_poly = match Polynomial::interpolate_fft::<F>(&fib_sq) {
         Ok(poly) => poly,
         Err(e) => panic!("{:?}", e),
     };
     assert_eq!(trace_poly.coefficients.len(), INT_DOM_SIZE);
+    assert_eq!(trace_poly.evaluate(&FE::from(1 as u64)), FE::from(1 as u64));
+    assert_eq!(trace_poly.evaluate(&F::get_primitive_root_of_unity(10 as u64).unwrap()), FE::from(3141592 as u64));
 }
