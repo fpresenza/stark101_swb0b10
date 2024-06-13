4871df9c60e4e110b5ac18cfd795424f041f22f4
fpresenza <francisco.presenza@gmail.com>
2024-06-13 16:54:03 -0300

    switch to stark252 prime

--- stark101/src/main.rs
+++ stark101/src/main.rs
@@ -1,10 +1,10 @@
-use lambdaworks_math::field::fields::u64_prime_field::U64FieldElement;
+use lambdaworks_math::field::fields::fft_friendly::stark_252_prime_field::Stark252PrimeField;
+use lambdaworks_math::field::element::FieldElement;
 use lambdaworks_math::polynomial::Polynomial;
 
-// modulus is 3221225473 = 3 * 2^30 + 1
-// construct associated prime field and field element types
-const MODULUS: u64 = 0b11000000000000000000000000000001;
-type FE = U64FieldElement<MODULUS>;
+// the stark252 field has 2-adicity of 192, i.e., the largest
+// multiplicative subgroup whose order is a power of two has order 2^192
+type FE = FieldElement<Stark252PrimeField>;
 
 // interpolation domain of size 1024 = 2^10
 const INT_DOM_SIZE: usize = 0b10000000000;
@@ -15,8 +15,8 @@ const EVAL_DOM_SIZE: usize = 0b10000000000000;
 fn main() {
     // create vec to hold fibonacci square sequence
     let mut fib_sq = Vec::<FE>::with_capacity(INT_DOM_SIZE);
-    fib_sq.push(FE::new(1));
-    fib_sq.push(FE::new(3141592));
+    fib_sq.push(FE::from(1 as u64));
+    fib_sq.push(FE::from(3141592 as u64));
 
     for i in 2..INT_DOM_SIZE {
         let x = fib_sq[i-2];
@@ -26,12 +26,12 @@ fn main() {
 
     // define generator of evaluation domain (order 2^10)
     // then construct evaluation domain
-    let generator_eval_dom = FE::new(1855261384);
+    let generator_eval_dom = FE::from(1855261384 as u64);
     let eval_dom = (0..INT_DOM_SIZE).map(|i| generator_eval_dom.pow(i)).collect::<Vec<FE>>();
     let trace_poly = match Polynomial::interpolate(&eval_dom, &fib_sq) {
         Ok(poly) => poly,
         Err(e) => panic!("{:?}", e),
     };
-    println!("{:?}", trace_poly.evaluate(&FE::new(1)));
-    println!("{:?}", trace_poly.evaluate(&generator_eval_dom));
+    println!("{:?}", trace_poly.evaluate(&FE::from(1 as u64)).representative());
+    println!("{:?}", trace_poly.evaluate(&generator_eval_dom).representative());
 }
