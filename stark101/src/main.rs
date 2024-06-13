9ac9163da40cc34465d6d41a39ec72afd45ec28a
fpresenza <francisco.presenza@gmail.com>
2024-06-13 11:51:11 -0300

    define field elements and initialize fibonacci square sequence

--- stark101/src/main.rs
+++ stark101/src/main.rs
@@ -1,3 +1,11 @@
+use lambdaworks_math::field::fields::u64_prime_field::U64FieldElement;
+
+const MODULUS: u64 = 0xc0000001;
+type FE = U64FieldElement<MODULUS>;    
+
 fn main() {
-    println!("Hello, world!");
+    // create vec to hold fibonacci square sequence
+    let mut fib_sq = Vec::<FE>::with_capacity(1024);
+    fib_sq.push(FE::new(1));
+    fib_sq.push(FE::new(3141592));
 }
