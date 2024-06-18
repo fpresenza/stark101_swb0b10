e3a04a6af0ff82f253db1a928574b1647d6e8559
fpresenza <francisco.presenza@gmail.com>
2024-06-18 12:19:40 -0300

    move public inputs outside prover + switch sha3 for keccak

--- stark101/src/main.rs
+++ stark101/src/main.rs
@@ -1,5 +1,49 @@
+use lambdaworks_math::field::{
+    traits::IsFFTField,
+    fields::montgomery_backed_prime_fields::IsModulus,
+    fields::fft_friendly::stark_252_prime_field::{
+        Stark252PrimeField,
+        MontgomeryConfigStark252PrimeField
+    },
+    element::FieldElement
+};
+
 mod prover;
 
+
+// the stark252 field has 2-adicity of 192, i.e., the largest
+// multiplicative subgroup whose order is a power of two has order 2^192
+type F = Stark252PrimeField;
+type FConfig = MontgomeryConfigStark252PrimeField;
+type FE = FieldElement<F>;
+
+// interpolation domain of size 1024 = 2^10
+const INT_DOM_SIZE: usize = 0b10000000000;
+const INT_DOM_SIZE_LOG2: u64 = 10;
+// evaluation domain of size 8192 = 2^13 (blow-up factor is 2^3)
+const EVAL_DOM_SIZE: usize = 0b10000000000000;
+
+
 fn main() {
-    prover::generate_proof();
+
+    // public input //
+    // field properties
+    let modulus = FConfig::MODULUS;
+    let g = F::get_primitive_root_of_unity(INT_DOM_SIZE_LOG2).unwrap();
+
+    // trace properties
+    let fib_squared_0 = FE::one();
+    let fib_squared_1022 = FE::from_hex_unchecked("6A317721EF632FF24FB815C9BBD4D4582BC7E21A43CFBDD89A8B8F0BDA68252");
+
+    let public_input = (
+        modulus,
+        INT_DOM_SIZE,
+        EVAL_DOM_SIZE,
+        g,
+        fib_squared_0,
+        fib_squared_1022,
+    );
+
+
+    prover::generate_proof(public_input.clone());
 }
--- stark101/src/prover.rs
+++ stark101/src/prover.rs
@@ -1,12 +1,16 @@
+use lambdaworks_math::unsigned_integer::element::U256;
 use lambdaworks_math::field::{
-    traits::IsFFTField,
-    fields::fft_friendly::stark_252_prime_field::Stark252PrimeField,
+    fields::fft_friendly::stark_252_prime_field::{
+        Stark252PrimeField
+    },
     element::FieldElement
 };
 use lambdaworks_math::polynomial::Polynomial;
-use lambdaworks_crypto::merkle_tree::{
-    merkle::MerkleTree,
-    backends::types::Sha3_256Backend
+use lambdaworks_crypto::{
+    merkle_tree::{
+        merkle::MerkleTree,
+        backends::types::Keccak256Backend
+    },
 };
 
 use stark101::poly;
@@ -16,34 +20,29 @@ use stark101::poly;
 type F = Stark252PrimeField;
 type FE = FieldElement<F>;
 
-// interpolation domain of size 1024 = 2^10
-const INT_DOM_SIZE: usize = 0b10000000000;
 
-// evaluation domain of size 8192 = 2^13 (blow-up factor is 2^3)
-const EVAL_DOM_SIZE: usize = 0b10000000000000;
-const BLOWUP_FACTOR: usize = 0b1000;
-
-pub fn generate_proof() {
+pub fn generate_proof(public_input: (U256, usize, usize, FE, FE, FE)) {
     // ===================================
     // ==========|    Part 1:   |=========
     // === Statement, LDE & Commitment ===
     // ===================================
+    // extract public input
+    let (modulus, int_dom_size, eval_dom_size, g, fib_squared_0, fib_squared_1022) = public_input;
     // define example parameters
     let one = FE::one();
-    let first_elem = one;
-    let witness_elem = FE::from(3141592_u64);
-    let result_elem = FE::from_hex_unchecked("6A317721EF632FF24FB815C9BBD4D4582BC7E21A43CFBDD89A8B8F0BDA68252");
-    let g = F::get_primitive_root_of_unity(10_u64).unwrap();
+    let witness = FE::from(3141592_u64);
+
+    // define primite root
     let g_to_the_1021 = g.pow(1021_u64);
     let g_to_the_1022 = g * g_to_the_1021;
     let g_to_the_1023 = g * g_to_the_1022;
 
     // create vec to hold fibonacci square sequence
-    let mut fib_squared = Vec::<FE>::with_capacity(INT_DOM_SIZE);
-    fib_squared.push(first_elem);
-    fib_squared.push(witness_elem);
+    let mut fib_squared = Vec::<FE>::with_capacity(int_dom_size);
+    fib_squared.push(fib_squared_0);
+    fib_squared.push(witness);
 
-    for i in 2..INT_DOM_SIZE {
+    for i in 2..int_dom_size {
         let x = fib_squared[i-2];
         let y = fib_squared[i-1];
         fib_squared.push(x.pow(2_u64) + y.pow(2_u64));
@@ -54,51 +53,52 @@ pub fn generate_proof() {
         Ok(p) => p,
         Err(e) => panic!("{:?}", e),
     };
-    assert_eq!(trace_poly.coefficients.len(), INT_DOM_SIZE);
-    assert_eq!(trace_poly.evaluate(&one), first_elem);
-    assert_eq!(trace_poly.evaluate(&g), witness_elem);
-    assert_eq!(trace_poly.evaluate(&g_to_the_1022), result_elem);
+    assert_eq!(trace_poly.coefficients.len(), int_dom_size);
+    assert_eq!(trace_poly.evaluate(&one), fib_squared_0);
+    assert_eq!(trace_poly.evaluate(&g), witness);
+    assert_eq!(trace_poly.evaluate(&g_to_the_1022), fib_squared_1022);
 
     // fft-evaluate the fibonacci square sequence over a larger domain
     // of size (blow-up factor) * (interpolation domain size)
     // the offset is obtained as an outside not in the interpolation domain
     let offset = FE::from(2_u64);
-    assert!(offset.pow(INT_DOM_SIZE as u64) != one);
+    assert!(offset.pow(int_dom_size as u64) != one);
     let trace_eval = match Polynomial::evaluate_offset_fft::<F>(
-        &trace_poly, 1, Some(EVAL_DOM_SIZE), &offset
+        &trace_poly, 1, Some(eval_dom_size), &offset
     ) {
         Ok(p) => p,
         Err(e) => panic!("{:?}", e),
     };
-    assert_eq!(trace_eval.len(), EVAL_DOM_SIZE);
+    assert_eq!(trace_eval.len(), eval_dom_size);
 
     // commit to the trace evaluations over the larger domain using a merkle tree
-    let trace_poly_merkle_tree = MerkleTree::<Sha3_256Backend<F>>::build(&trace_eval);
+    let trace_poly_merkle_tree = MerkleTree::<Keccak256Backend<F>>::build(&trace_eval);
+    // println!("{:?}", trace_poly_merkle_tree.root);
 
     // ===================================
     // =========|    Part 2:   |==========
     // ===== Polynomial Constraints ======
     // ===================================
     let x = Polynomial::new_monomial(one, 1);
-    let x_to_the_1024 = Polynomial::new_monomial(one, 1024);
+    let x_to_the_1024 = Polynomial::new_monomial(one, int_dom_size);
 
     // initial element constraint
     let initial_constraint_poly = poly::polynomial_division(
-        &(&trace_poly - first_elem),
+        &(&trace_poly - fib_squared_0),
         &(&x - one),
-        EVAL_DOM_SIZE,
+        eval_dom_size,
         &offset
     );
-    assert_eq!(initial_constraint_poly.coefficients.len(), INT_DOM_SIZE - 1);
+    assert_eq!(initial_constraint_poly.coefficients.len(), int_dom_size - 1);
 
     // result element constraint
     let result_constraint_poly = poly::polynomial_division(
-        &(&trace_poly - result_elem),
+        &(&trace_poly - fib_squared_1022),
         &(&x - g_to_the_1022),
-        EVAL_DOM_SIZE,
+        eval_dom_size,
         &offset
     );
-    assert_eq!(result_constraint_poly.coefficients.len(), INT_DOM_SIZE - 1);
+    assert_eq!(result_constraint_poly.coefficients.len(), int_dom_size - 1);
 
     // trace transition constraint
     // numerator
@@ -107,17 +107,17 @@ pub fn generate_proof() {
     let trace_poly_squared = poly::polynomial_power(
         &trace_poly,
         2_u64,
-        EVAL_DOM_SIZE,
+        eval_dom_size,
         &offset
     );
     let trace_poly_scaled_once_squared = poly::polynomial_power(
         &trace_poly_scaled_once,
         2_u64,
-        EVAL_DOM_SIZE,
+        eval_dom_size,
         &offset
     );
-    assert!(trace_poly_squared.coefficients.len() <= 2 * INT_DOM_SIZE);
-    assert!(trace_poly_scaled_once_squared.coefficients.len() <= 2 * INT_DOM_SIZE);
+    assert!(trace_poly_squared.coefficients.len() <= 2 * int_dom_size);
+    assert!(trace_poly_scaled_once_squared.coefficients.len() <= 2 * int_dom_size);
     assert_eq!(
         trace_poly_scaled_twice.evaluate(&g_to_the_1021),
         trace_poly_scaled_once_squared.evaluate(&g_to_the_1021) + trace_poly_squared.evaluate(&g_to_the_1021)
@@ -130,7 +130,7 @@ pub fn generate_proof() {
             &(&x - g_to_the_1022),
             &(&x - g_to_the_1023)
         ],
-        EVAL_DOM_SIZE,
+        eval_dom_size,
         &offset
     );
     // denominator
@@ -139,8 +139,8 @@ pub fn generate_proof() {
     let transition_constraint_poly = poly::polynomial_division(
         &numerator,
         &denominator,
-        EVAL_DOM_SIZE,
+        eval_dom_size,
         &offset
     );
-    assert!(transition_constraint_poly.coefficients.len() <= 2 * INT_DOM_SIZE);
+    assert!(transition_constraint_poly.coefficients.len() <= 2 * int_dom_size);
 }
