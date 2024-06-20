8e488ccefa92fc06a6747267335d9d6c34cadf26
fpresenza <francisco.presenza@gmail.com>
2024-06-20 16:38:54 -0300

    queries for upper layers + transcript append outside functions

--- stark101/src/fri.rs
+++ stark101/src/fri.rs
@@ -91,7 +91,8 @@ pub fn commit_and_fold<F>(
     let mut fri_layers = Vec::<FriLayer<F>>::with_capacity(number_of_layers);
 
     // commit to evaluations
-    let (eval, tree) = commit(&polynomial, domain_size, &offset, transcript);
+    let (eval, tree) = commit(&polynomial, domain_size, &offset);
+    transcript.append_bytes(&tree.root);
     println!(
         "Layer 0: \n \t Appending root of composition polynomial (degree {:?}) to transcript.",
          polynomial.degree()
@@ -104,7 +105,7 @@ pub fn commit_and_fold<F>(
     // get queries evaluations,add to transcript and generate inclusion proofs
     println!("\t Appending query indices to transcript.");
     let queries = query_indices.iter().map(|i| { 
-        let idx = *i as usize;
+        let idx = i.to_owned();
         let sym_idx = (idx + domain_size / 2) % domain_size;
         transcript.append_bytes(&idx.to_be_bytes());
 
@@ -134,19 +135,35 @@ pub fn commit_and_fold<F>(
         domain_size /= 2;
         offset = offset.square();
 
-        let (eval, tree) = commit(&polynomial, domain_size, &offset, transcript);
+        let (eval, tree) = commit(&polynomial, domain_size, &offset);
+        transcript.append_bytes(&tree.root);
         println!(
             "Layer {:?}: \n \t Appending root of folded polynomial (degree {:?}) to transcript.",
             layer,
             polynomial.degree()
         );
 
-        // fri_layers.push(
-        //     FriLayer::<F>::new(
-        //         root,
-        //         vec![Query::<F>::new(None, None, FieldElement::one())]
-        //     )
-        // );
+        // get queries evaluations,add to transcript and generate inclusion proofs
+        let queries = query_indices.iter().map(|i| { 
+            let idx = i.to_owned() % domain_size;
+            let sym_idx = (idx + domain_size / 2) % domain_size;
+            // transcript.append_bytes(&idx.to_be_bytes());
+
+            Query::for_folded_layer(
+                tree.get_proof_by_pos(idx).unwrap(),
+                eval[sym_idx].to_owned(),
+                tree.get_proof_by_pos(sym_idx).unwrap()
+            )
+        })
+        .collect::<Vec<Query<F>>>();
+
+        // append layer
+        fri_layers.push(
+            FriLayer::<F>::new(
+                tree.root,
+                queries
+            )
+        );
     }
 
     let constant_poly = polynomial.coefficients.first().unwrap();
@@ -159,8 +176,7 @@ pub fn commit_and_fold<F>(
 fn commit<F>(
         polynomial: &Polynomial<FieldElement<F>>,
         domain_size: usize,
-        offset: &FieldElement<F>,
-        transcript: &mut DefaultTranscript<F>
+        offset: &FieldElement<F>
     ) -> (Vec<FieldElement<F>>, MerkleTree<Keccak256Backend<F>>)
     where
         F: IsField + IsFFTField,
@@ -174,7 +190,6 @@ fn commit<F>(
     ).unwrap();
     
     let tree = MerkleTree::<Keccak256Backend<F>>::build(&eval);
-    transcript.append_bytes(&tree.root);
 
     (eval, tree)
 }
@@ -183,12 +198,12 @@ fn sample_queries<F>(
         num_queries: usize,
         domain_size: usize,
         transcript: &mut DefaultTranscript<F>
-    ) -> Vec<u64> 
+    ) -> Vec<usize> 
     where 
         F: IsField,
         FieldElement<F>: AsBytes + ByteConversion {
 
-    let mut query_indices = Vec::<u64>::with_capacity(num_queries);
+    let mut query_indices = Vec::<usize>::with_capacity(num_queries);
 
     for _ in 0..num_queries {
         let (_, query_index) = U256::from_bytes_be(
@@ -197,7 +212,7 @@ fn sample_queries<F>(
         .div_rem(
             &U256::from(domain_size as u64)
         );
-        query_indices.push(query_index.limbs[3]);
+        query_indices.push(query_index.limbs[3] as usize);
     }
     query_indices
 }
